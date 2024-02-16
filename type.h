5c582b193da7a4598321ee1447c8db1220d31922
Sihan_Zhang <31116661+dazzleeee@users.noreply.github.com>
2024-02-16 20:37:37 +0800

    Create type.h

---
+++ type.h
@@ -0,0 +1,11 @@
+typedef char int8_t;
+typedef unsigned char uint8_t;
+
+typedef short int16_t;
+typedef unsigned short uint16_t;
+
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+typedef long long int64_t;
+typedef unsigned long long uint64_t;   #通过这样定义不同数据类型的长度，使得移植性提高
