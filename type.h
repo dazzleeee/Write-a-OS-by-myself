05e66255d01f58e58b73c3670fca255844ef1886
Sihan_Zhang <31116661+dazzleeee@users.noreply.github.com>
2024-02-17 19:20:50 +0800

    Update type.h

--- type.h
+++ type.h
@@ -1,3 +1,7 @@
+// os/types.h
+#ifndef __TYPES_H__   #防御性编程，防止重复定义
+#define __TYPES_H__
+
 typedef char int8_t;
 typedef unsigned char uint8_t;
 
@@ -8,4 +12,11 @@ typedef int int32_t;
 typedef unsigned int uint32_t;
 
 typedef long long int64_t;
-typedef unsigned long long uint64_t;   #通过这样定义不同数据类型的长度，使得移植性提高
+typedef unsigned long long uint64_t;
+
+#endif
+
+作者：熊猫阿宝
+链接：https://zhuanlan.zhihu.com/p/390428613
+来源：知乎
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
